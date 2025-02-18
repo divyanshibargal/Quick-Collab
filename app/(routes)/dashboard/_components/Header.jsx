20c6c81fa1895b37aefc27891c3c500cd6b16e72
Divyanshi Bargal <divyanshibargal711@gmail.com>
2025-02-18 19:20:09 +0530

    Organisation Created

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,11 +1,20 @@
-
+"use client"
 import React from 'react'
 import Logo from './Logo'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+import { useAuth } from '@clerk/clerk-react'
 
 const Header = () => {
+  const{orgId}=useAuth();
+  console.log(orgId);
   return (
-    <div>
+    <div className='flex justify-between items-center p-3 shadow-sm'>
     <Logo/>
+    <OrganizationSwitcher 
+    afterLeaveOrganizationUrl={'/dashboard'}
+    afterCreateOrganizationUrl={'/dashboard'}
+    />
+    <UserButton/>
     </div>
   )
 }
--- app/(routes)/dashboard/_components/Logo.jsx
+++ app/(routes)/dashboard/_components/Logo.jsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
-import React from 'react'
 
 const Logo = () => {
   return (
-    <div>
-      <Image src={'/Main_logo.png'} alt='logo' width={100} height={50}/>
+    <div className='flex items-center gap-2'>
+      <Image src={'/QuickCollab_logo.png'} alt='logo' width={100} height={50}/>
     </div>
   )
 }
