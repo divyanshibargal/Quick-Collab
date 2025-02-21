04b6b74cceee273b0d99004829ab8bd3c66a29a0
Divyanshi Bargal <divyanshibargal711@gmail.com>
2025-02-21 22:21:11 +0530

    Auth Updated

--- app/(routes)/createworkspace/page.js
+++ app/(routes)/createworkspace/page.js
@@ -1,19 +1,58 @@
-'use client'
-import Image from 'next/image'
-import React, { useState } from 'react'
+"use client";
+import { SmilePlus } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+import { Button } from "../../../components/ui/button";
+import CoverPicker from "../../../components/ui/CoverPicker";
+import { Input } from "../../../components/ui/input";
 
 function CreateWorkspace() {
-    const[coverImage, setcoverImage]=useState('/cover.png')
+const [coverImage, setcoverImage] = useState("/cover.png");
+const [workspaceName,setWorkspaceName]=useState();
+
 return (
-    <div className='p-10 md:px-36 lg:px-52 xl:px-80 py-20'>
-        <div>
-            <div>
-                <Image src={coverImage} width={400} height={400} className='w-full h-[150px] object-cover
-                rounded-t-xl' />
-            </div>
+    <div className="p-10 md:px-36 lg:px-64 xl:px-96 py-28">
+    <div className="shadow-2xl rounded-xl">
+        {/* cover image */}
+        <CoverPicker/>
+        <div className="relative group">
+        <h2 className="hidden absolute p-4 w-full h-full items-center group-hover:flex justify-center">
+            Change cover
+        </h2>
+        <div className="group-hover:opacity-40 ">
+            <Image
+            src={coverImage}
+            width={400}
+            height={400}
+            className="w-full h-[150px] object-cover
+                rounded-t-xl"
+            alt="CoverImage"
+            />
+        </div>
+        </div>
+        {/* Input section */}
+        <div className="p-12">
+        <h2 className="font-semibold text-xl ">Create a new Workspace</h2>
+        <h2 className="text-sm mt-2">
+            This is a shared Workspace where you can collaborate with your team.
+            You can always rename it later.
+        </h2>
+        <div className="mt-8 flex gap-2 items-center">
+            <Button variant="outline">
+            <SmilePlus />
+            </Button>
+            <Input placeholder=" Workspace Name"
+            onChange={(e)=>setWorkspaceName(e.target.value)}
+            />
+        </div>
+        <div className="mt-7 flex justify-end gap-6">
+            <Button disabled={!workspaceName?.length}>Create</Button>
+            <Button variant="outline">Cancel</Button>
+        </div>
         </div>
     </div>
-)
+    </div>
+);
 }
 
-export default CreateWorkspace
+export default CreateWorkspace;
