b8dbb46cb8f9c129a8e16e2119eb7a1bd0768292
Divyanshi Bargal <divyanshibargal711@gmail.com>
2025-02-21 18:16:40 +0530

    Cover Image Added

--- app/(routes)/createworkspace/page.js
+++ app/(routes)/createworkspace/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+'use client'
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 function CreateWorkspace() {
+    const[coverImage, setcoverImage]=useState('/cover.png')
 return (
-    <div>
-        CreateWorkspace
+    <div className='p-10 md:px-36 lg:px-52 xl:px-80 py-20'>
+        <div>
+            <div>
+                <Image src={coverImage} width={400} height={400} className='w-full h-[150px] object-cover
+                rounded-t-xl' />
+            </div>
+        </div>
     </div>
 )
 }
